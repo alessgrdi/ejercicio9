32ef0ffebc3be2641d4695b2a77e2ca568d72910
alessgrdi <alejandrogrageradiaz@hotmail.com>
2015-08-16 20:39:19 +0200

    control sessions

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var partials = require('express-partials');
+var session = require('express-session');
 
 var routes = require('./routes/index');
 //var users = require('./routes/users');
+
+var isFromSession = false;
 
 var app = express();
 
@@ -22,11 +25,56 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 //app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.urlencoded());
-app.use(cookieParser());
+app.use(cookieParser('Quiz 2015'));
+//app.use(session());
+app.use(session({
+            name: 'quiz-2015', // configuración de la cookie
+           secret: 'huertix',
+            resave: true,       // Forces the session to be saved back to the session store
+            rolling: true,      // Force a cookie to be set on every response. This resets the expiration date.
+            saveUninitialized: false,       //
+            cookie: { maxAge: 120000}  // Tiempo de la sesion, expiración de la cookie -> 30000 = 30s.
+            }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+//app.use('/', routes);
 //app.use('/users', users);
+
+// Helpers dinamicos:
+app.use(function(req, res, next) {
+
+  req.session.touch();
+  res.locals.session = req.session;
+
+  // si no existe lo inicializa
+  if (!req.session.redir) {
+    req.session.redir = '/';
+  }
+  // guardar path en session.redir para despues de login
+  if (!req.path.match(/\/login|\/logout/)) {
+    req.session.redir = req.path;
+  }
+
+  if(req.session.user){
+        isFromSession = true;
+        console.log("en sesión");
+
+    }else{
+        if(isFromSession){
+            isFromSession = false;
+            console.log("fuera de la sesión");
+            var err = new Error('Sesión Finalizada');
+           err.status = 1001;
+            next(err);
+        }
+     }
+
+  // Hacer visible req.session en las vistas
+  //res.locals.session = req.session;
+  next();
+});
+
+app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var quizController = require('../controllers/quiz_controller');
+var sessionController = require('../controllers/session_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -17,7 +18,12 @@ router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 router.get('/quizes/new',                  quizController.new);
 router.post('/quizes/create',              quizController.create);
 router.delete('/quizes/:quizId(\\d+)',     quizController.destroy);
-router.get('/quizes/buscar',                      quizController.buscar);
+router.get('/quizes/buscar',               quizController.buscar);
+
+// Definición de rutas de sesion
+router.get('/login',  sessionController.new);     // formulario login
+router.post('/login', sessionController.create);  // crear sesión
+router.get('/logout', sessionController.destroy); // destruir sesión
 
 
 module.exports = router;
